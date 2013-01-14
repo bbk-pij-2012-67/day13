ba38cefafc4e63b54907c0e3565075053690814e
Edmund White <edmund.white_at_hotmaildotcom>
2013-01-14 20:12:05 +0000

    Initial

---
+++ LinkedListRunner.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class LinkedListRunner{
+	
+	public static void main(String[] args){
+		
+		List<String> list = new LinkedList<>();
+		list.add("aaa");
+		list.add("bbb");
+		list.add("ccc");
+		list.add("ddd");
+		list.add("eee");
+		list.add("fff");
+		list.add("ggg");
+		list.add("hhh");
+		list.add("iii");
+		
+	}
